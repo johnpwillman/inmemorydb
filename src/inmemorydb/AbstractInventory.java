8734bc6d9630a2434a9fa576f72a0da856a9d26b
John Willman <johnpwillman@gmail.com>
2014-04-06 17:53:45 -0700

    Expand method list for Abstract Inventory. Add Inventory Decorator.

--- src/inmemorydb/AbstractInventory.java
+++ src/inmemorydb/AbstractInventory.java
@@ -4,11 +4,21 @@ public abstract class AbstractInventory {
 	
 	public abstract int addMovie(String movieName, double moviePrice, int movieQty);
 	
+	public abstract String getMovieName(int movieID);
+	
 	public abstract void sellMovie(int movieID);
+	
+	public abstract int getMovieQty(int movieID);
+	
+	public abstract int getMovieQty(String movieName);
 	
 	public abstract void addMovieQty(int movieID, int qtyToAdd);
 	
 	public abstract void setMoviePrice(int movieID, double newPrice);
+	
+	public abstract double getMoviePrice(int movieID);
+	
+	public abstract double getMoviePrice(String movieName);
 	
 	public abstract Memento saveToMemento();
 	
---
+++ src/inmemorydb/InventoryDecorator.java
@@ -0,0 +1,67 @@
+package inmemorydb;
+
+public class InventoryDecorator extends AbstractInventory {
+	
+	private Inventory decoratedInventory;
+	
+	public InventoryDecorator(Inventory inventoryToDecorate) {
+		decoratedInventory = inventoryToDecorate;
+	}
+
+	@Override
+	public int addMovie(String movieName, double moviePrice, int movieQty) {
+		return decoratedInventory.addMovie(movieName, moviePrice, movieQty);
+	}
+
+	@Override
+	public String getMovieName(int movieID) {
+		return decoratedInventory.getMovieName(movieID);
+	}
+
+	@Override
+	public void sellMovie(int movieID) {
+		decoratedInventory.sellMovie(movieID);
+	}
+
+	@Override
+	public int getMovieQty(int movieID) {
+		return decoratedInventory.getMovieQty(movieID);
+	}
+
+	@Override
+	public int getMovieQty(String movieName) {
+		return decoratedInventory.getMovieQty(movieName);
+	}
+
+	@Override
+	public void addMovieQty(int movieID, int qtyToAdd) {
+		decoratedInventory.addMovieQty(movieID, qtyToAdd);
+		
+	}
+
+	@Override
+	public void setMoviePrice(int movieID, double newPrice) {
+		decoratedInventory.setMoviePrice(movieID, newPrice);
+	}
+
+	@Override
+	public double getMoviePrice(int movieID) {
+		return decoratedInventory.getMoviePrice(movieID);
+	}
+
+	@Override
+	public double getMoviePrice(String movieName) {
+		return decoratedInventory.getMoviePrice(movieName);
+	}
+
+	@Override
+	public Memento saveToMemento() {
+		return decoratedInventory.saveToMemento();
+	}
+
+	@Override
+	public void restoreFromMemento(Memento memento) {
+		decoratedInventory.restoreFromMemento(memento);
+	}
+	
+}
